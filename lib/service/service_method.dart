73638dd07e6374dd53ab3acdec9471d4cf253dbd
AndrewZhang <zzq.happy@qq.com>
2019-03-13 17:21:19 +0800

    首页细节优化

--- lib/service/service_method.dart
+++ lib/service/service_method.dart
@@ -5,7 +5,7 @@ import 'service_url.dart';
 
 Future request(url, {formData}) async {
   try {
-    print('开始获取数据..........');
+    print('开始获取数据$url..........');
     Response response;
 
     Dio dio = new Dio();
@@ -20,7 +20,7 @@ Future request(url, {formData}) async {
     if (response.statusCode == 200) {
       return response.data;
     } else {
-      throw Exception('后端接口出现异常.');
+      throw Exception('后端接口出现异常$url.');
     }
   } catch(e) {
     return print('ERROR:==========>$e');

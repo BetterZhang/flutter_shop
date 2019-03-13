f52d1cb13c22bc54feaaae6b21254a24534cf749
AndrewZhang <zzq.happy@qq.com>
2019-03-13 15:08:24 +0800

    json解析和复杂数据模型转换

--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../service/service_method.dart';
 import 'dart:convert';
+import '../model/category.dart';
 
 class CategoryPage extends StatefulWidget {
   @override
@@ -12,7 +13,8 @@ class _CategoryPageState extends State<CategoryPage> {
   void _getCategory() async {
     await request('getCategory').then((val) {
       var data = json.decode(val.toString());
-      print(data);
+      CategoryBigListModel list = CategoryBigListModel.fromJson(data['data']);
+      list.data.forEach((item) => print(item.mallCategoryName));
     });
   }
   

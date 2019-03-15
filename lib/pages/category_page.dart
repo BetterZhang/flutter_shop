7a1f09daa1e11207985a3200da9ec691f5336f58
AndrewZhang <zzq.happy@qq.com>
2019-03-15 16:20:52 +0800

    分类页面左侧导航菜单

--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../service/service_method.dart';
 import 'dart:convert';
 import '../model/category.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CategoryPage extends StatefulWidget {
   @override
@@ -9,22 +10,94 @@ class CategoryPage extends StatefulWidget {
 }
 
 class _CategoryPageState extends State<CategoryPage> {
-  
-  void _getCategory() async {
-    await request('getCategory').then((val) {
-      var data = json.decode(val.toString());
-      CategoryBigListModel list = CategoryBigListModel.fromJson(data['data']);
-      list.data.forEach((item) => print(item.mallCategoryName));
-    });
-  }
-  
+
   @override
   Widget build(BuildContext context) {
-    _getCategory();
-    return Container(
-      child: Center(
-        child: Text('分类页面'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('商品分类')
+      ),
+      body: Container(
+        child: Row(
+          children: <Widget>[
+            LeftCategoryNav(),
+          ],
+        ),
       ),
     );
   }
 }
+
+// 左侧导航菜单
+class LeftCategoryNav extends StatefulWidget {
+  @override
+  _LeftCategoryNavState createState() => _LeftCategoryNavState();
+}
+
+class _LeftCategoryNavState extends State<LeftCategoryNav> {
+
+  List list = [];
+
+  @override
+  void initState() {
+    _getCategory();
+    super.initState();
+  }
+
+  void _getCategory() async {
+    await request('getCategory').then((val) {
+      var data = json.decode(val.toString());
+      CategoryModel category = CategoryModel.fromJson(data);
+      setState(() {
+        list = category.data;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: ScreenUtil().setWidth(180),
+      decoration: BoxDecoration(
+        border: Border(
+          right: BorderSide(
+            width: 1,
+            color: Colors.black12
+          )
+        ),
+      ),
+      child: ListView.builder(
+        itemCount: list.length,
+        itemBuilder: (context, index) {
+          return _leftInkWell(index);
+        },
+      ),
+    );
+  }
+
+  Widget _leftInkWell(int index) {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        height: ScreenUtil().setHeight(100),
+        padding: EdgeInsets.only(left: 10, top: 20),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(
+            bottom: BorderSide(
+              width: 1,
+              color: Colors.black12
+            )
+          )
+        ),
+        child: Text(
+          list[index].mallCategoryName,
+          style: TextStyle(
+            fontSize: ScreenUtil().setSp(26)
+          ),
+        ),
+      ),
+    );
+  }
+}
+

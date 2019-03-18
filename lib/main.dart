3f400933797a478e6ee9b0ffb7bd67e4dcb28b86
AndrewZhang <zzq.happy@qq.com>
2019-03-18 15:41:36 +0800

    provide状态管理

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:provide/provide.dart';
 import './pages/index_page.dart';
+import './provide/counter.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  var counter = Counter();
+  var providers = Providers();
+  providers
+    ..provide(Provider<Counter>.value(counter));
+
+  runApp(
+    ProviderNode(
+      child: MyApp(), 
+      providers: providers
+     ));
+}
 
 class MyApp extends StatelessWidget {
   @override

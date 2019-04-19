da9f1d8f96d90c0efe317efadc7f86d8e551af8d
AndrewZhang <zzq.happy@qq.com>
2019-04-19 17:32:19 +0800

    flutter路由fluro的引入和商品详细页建立

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import './pages/index_page.dart';
 import './provide/counter.dart';
 import './provide/child_category.dart';
 import './provide/category_goods_list.dart';
+import 'package:fluro/fluro.dart';
 
 void main() {
   var counter = Counter();

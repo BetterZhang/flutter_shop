aea5e3cdc5f429c3c39ea8bf2724939d7809ea43
AndrewZhang <zzq.happy@qq.com>
2019-06-11 17:10:44 +0800

    购物车_制作数量加减按钮UI

--- lib/pages/cart_page/cart_item.dart
+++ lib/pages/cart_page/cart_item.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../model/cartInfo.dart';
+import './cart_count.dart';
 
 class CartItem extends StatelessWidget {
 
@@ -71,6 +72,7 @@ class CartItem extends StatelessWidget {
       child: Column(
         children: <Widget>[
           Text(item.goodsName),
+          CartCount(),
         ],
       ),
     );
